ec8a8efacac66adddf3ad9c295f2516d3d73a57c
suphideploy <chingchimplee@gmail.com>
2023-08-15 18:53:02 +0700

    First Ecommerce with Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens:{
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200'
+    },
+    container: {
+      center: true,
+      padding: '1rem'
+    },
+    extend: {
+      fontFamily:{
+        poppins: "'Poppins', sans-serif",
+        roboto: "'Roboto', sans-serif"
+      },
+      colors:{
+          'primary': '#FD3D57'
+      }
+    },
+  },
+  plugins: [],
+}
+
